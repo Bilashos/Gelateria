993c0c62f8c1e4388b271374a059cb13d699e52a
albi98 <albyantox@gmail.com>
2025-04-14 19:02:50 +0200

    Revert "correzione bug e ralazione aggiunta"
    This reverts commit 0365dcc132f8775ad78923dc7827a3cd6ff916a2.

--- frontend/src/app/components/product/product-card/product-card.component.ts
+++ frontend/src/app/components/product/product-card/product-card.component.ts
@@ -137,7 +137,7 @@ export class ProductCardComponent {
   deleteProduct() {
     this.dialog.open(DialogConfermaComponent, {
       data: {
-        type: "strong_warning",
+        type: "confirmation",
         message: "Confermi di voler eliminare il prodotto: '" + this.product.name + "'",
         secondaryMessage: "Verranno eliminate tutte le informazioni."
       }
